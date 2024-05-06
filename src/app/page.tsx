dec3d20ded0daafb8e7d7f2d016fac82fbcf8adc
Hlib Dmytriiev <action.dmitrieff@gmail.com>
2024-05-06 13:56:53 +0300

    basic logic for backend request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [input, setInput] = useState<string>("");
-  const [] = useState<{
+  const [searchResults, setSearchResults] = useState<{
     results: string[];
     duration: number;
   }>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!input) return setSearchResults(undefined);
+
+      const res = await fetch(`/api/search?q=${input}`);
+    };
+
+    fetchData();
+  }, [input]);
 
   return (
     <div>
